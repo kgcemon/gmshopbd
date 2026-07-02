2b0e31f2aae6bb609584921384275679b065b88f
kgcemon <kgcemonbd@gmail.com>
2026-07-02 01:25:33 -0700

    updated

--- config/auth.php
+++ config/auth.php
@@ -42,8 +42,8 @@ return [
         ],
 
         'admin' => [
-            'driver' => 'session', // যদি শুধু admin panel হয়
-            'provider' => 'admins',
+            'driver' => 'session',
+            'provider' => 'users',
         ],
     ],
 
